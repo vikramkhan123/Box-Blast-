24a69f2a95b80c9e4c80b71945c61233e003b615
vikramkhan123 <138840567+vikramkhan123@users.noreply.github.com>
2026-08-05 14:54:37 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
+    
     buildFeatures {
         compose = true
     }
