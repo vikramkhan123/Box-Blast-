7a24c5a1d488d5773f2d6ca93c872c59db607217
vikramkhan123 <138840567+vikramkhan123@users.noreply.github.com>
2026-08-05 15:54:28 +0530

    Update AdventureGameActivity.kt

--- app/src/main/java/com/example/AdventureGameActivity.kt
+++ app/src/main/java/com/example/AdventureGameActivity.kt
@@ -1,1 +1,15 @@
+package com.example
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class AdventureGameActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        
+        val gameView = AdventureGameView(this)
+        setContentView(gameView)
+    }
+}
