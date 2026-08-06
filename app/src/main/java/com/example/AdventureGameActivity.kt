4e009d729c4bbf26cf210fac8134c02c860bfd94
vikramkhan123 <138840567+vikramkhan123@users.noreply.github.com>
2026-08-06 13:11:57 +0530

    Update AdventureGameActivity.kt

--- app/src/main/java/com/example/AdventureGameActivity.kt
+++ app/src/main/java/com/example/AdventureGameActivity.kt
@@ -5,11 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
 class AdventureGameActivity : ComponentActivity() {
+    private lateinit var gameView: AdventureGameView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        
-        val gameView = AdventureGameView(this)
+        gameView = AdventureGameView(this)
         setContentView(gameView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gameView.soundManager.playBGM()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gameView.soundManager.pauseBGM()
     }
 }
