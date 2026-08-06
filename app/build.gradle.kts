c0791d6f8b406e85ba0914ead47f98320e3c421c
vikramkhan123 <138840567+vikramkhan123@users.noreply.github.com>
2026-08-06 17:14:00 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+implementation("com.google.android.gms:play-services-ads:23.0.0")
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
