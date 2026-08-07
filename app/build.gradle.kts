708c1473bcc563b8354f0bdd18dbe868c556e4ca
vikramkhan123 <138840567+vikramkhan123@users.noreply.github.com>
2026-08-07 18:06:11 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example"
+        applicationId = "com.vikramlilawat.boxblast"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
